74e67f3c3a7f40302f48fb4a42ffac3582b10c09
BuildTools <florian.graziani@sfr.fr>
2025-04-29 10:17:28 +0200

    Mise à jour du projet

--- src/destination.ts
+++ src/destination.ts
@@ -2,9 +2,9 @@ import "./style.css"
 
 document.getElementById("app")!.innerHTML = `
     <div class="min-h-screen bg-black text-white font-montserrat flex flex-col">
-        <header class="container mx-auto relative md:flex justify-between items-center top-8 md:h-34">
-            <h1 class="hidden md:block">Agence2Voyage</h1>
-            <img alt="Logo" class="w-22 md:w-33 mx-auto md:absolute md:top-0 md:left-1/2 md:-translate-x-1/2"
+        <header class="container mx-auto relative md:flex justify-between items-center top-8">
+            <h1 class="hidden md:block md:py-0">Agence2Voyage</h1>
+            <img alt="Logo" class="w-12.5 mx-auto rounded-full"
                  src="/images/logo.png">
         
             <!--Menu PC-->
@@ -28,23 +28,119 @@ document.getElementById("app")!.innerHTML = `
             </button>
         
             <!--Menu mobile-->
-            <nav class="md:hidden fixed top-0 left-0 right-0 bottom-0 bg-black hidden" id="phone-menu">
+            <nav class="md:hidden fixed top-0 left-0 right-0 bottom-0 bg-black hidden z-50" id="phone-menu">
                 <button class="absolute right-10 top-15" id="close">
                     <img alt="Cross Icon" class="w-15" src="/images/cross.svg"/>
                 </button>
             </nav>
         </header>
         <main class="container mx-auto grow">
-            <section id="destinations" class="mt-40">
-                <div class="flex flex-wrap justify-between pb-20">
-                    <img src="/images/Tibet/Tibet%201.png" class="mx-auto md:m-0" alt="Tibet">
-                    <div class="flex flex-col text-center pt-5  md:pt-0">
+            <section class="mt-40" id="destinations">
+                <!-- Tibet -->
+                <div class="grid md:grid-cols-2 gap-10 justify-between pb-20">
+                    <!-- Carousel Tibet -->
+                    <div class="relative w-full" data-carousel="slide" id="tibet">
+                        <!-- Carousel wrapper-->
+                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                            <!-- Item 1 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="/images/Tibet/Tibet%20(1).jpg" alt="Tibet (1)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                            <!-- Item 2 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="/images/Tibet/Tibet%20(2).jpg" alt="Tibet (2)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                            <!-- Item 3 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="/images/Tibet/Tibet%20(3).jpg" alt="Tibet (3)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                            <!-- Item 4 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="/images/Tibet/Tibet%20(4).jpg" alt="Tibet (4)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                        </div>
+                        <!-- Slider indicators -->
+                        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                            <button aria-current="true" aria-label="Slide 1" class="w-3 h-3 rounded-full" data-carousel-slide-to="0" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 2" class="w-3 h-3 rounded-full" data-carousel-slide-to="0" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 3" class="w-3 h-3 rounded-full" data-carousel-slide-to="0" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 3" class="w-3 h-3 rounded-full" data-carousel-slide-to="0" type="button"></button>
+                        </div>
+                        <!-- Slider controls -->
+                        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                                </svg>
+                                <span class="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                </svg>
+                                <span class="sr-only">Next</span>
+                            </span>
+                        </button>
+                    </div>
+                    <div class="flex flex-col text-center pt-5 md:pt-0">
                         <p>Destination 1</p>
                         <p class="text-2xl flex items-center justify-center grow">Nous pouvons par exemple proposer une destination au Tibet</p>
                     </div>
                 </div>
-                <div class="mb-20">
-                    <img src="/images/Seychelles/Seychelles%201.png" alt="Seychelles" class="mx-auto md:m-0">
+                <!-- Seychelles -->
+                <div class="mb-20 grid md:grid-cols-2 gap-10">
+                    <!-- Carousel Seychelles -->
+                    <div class="relative w-full" data-carousel="slide" id="seychelles">
+                        <!-- Carousel wrapper -->
+                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                             <!-- Item 1 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img alt="Seychelles (1)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/images/Seychelles/Seychelles%20(1).jpg">
+                            </div>
+                            <!-- Item 2 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img alt="Seychelles (2)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/images/Seychelles/Seychelles%20(2).jpg">
+                            </div>
+                            <!-- Item 3 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img alt="Seychelles (3)" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/images/Seychelles/Seychelles%20(3).jpg">
+                            </div>
+                            <!-- Item 4 -->
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="/images/Seychelles/Seychelles%20(4).jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Seychelles (4)">
+                            </div>
+                        </div>
+                        <!-- Slider indicators -->
+                        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                            <button aria-current="true" aria-label="Slide 1" class="w-3 h-3 rounded-full" data-carousel-slide-to="0" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 2" class="w-3 h-3 rounded-full" data-carousel-slide-to="1" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 3" class="w-3 h-3 rounded-full" data-carousel-slide-to="2" type="button"></button>
+                            <button aria-current="false" aria-label="Slide 4" class="w-3 h-3 rounded-full" data-carousel-slide-to="3" type="button"></button>
+                        </div>
+                        <!-- Slider controls -->
+                        <button class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev type="button">
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg aria-hidden="true" class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" fill="none" viewBox="0 0 6 10" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5 1 1 5l4 4" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                </svg>
+                                <span class="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next type="button">
+                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg aria-hidden="true" class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" fill="none" viewBox="0 0 6 10" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="m1 9 4-4-4-4" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                </svg>
+                                <span class="sr-only">Next</span>
+                            </span>
+                        </button>
+                    </div>
+                    <div class="flex flex-col text-center ">
+                        <p>Destination 2</p>
+                        <p class="text-2xl flex items-center justify-center grow">Autre destination disponible : Les seychelles</p>
+                    </div>
                 </div>
             </section>
         </main>
